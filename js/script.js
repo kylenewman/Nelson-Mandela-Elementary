9129d51bc9baa6223e215c7a548392c213934a7f
kylenewman <kndesign@yahoo.com>
2016-03-20 21:12:23 -0500

    add slider to index

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 $(document).ready( function() {
    $("#nav-brand").hide(); //hide your div initially
-   var topOfOthDiv = $(".carousel").offset().top;
+   var topOfOthDiv = $("#carousel").offset().top;
    $(window).scroll(function() {
        if($(window).scrollTop() > topOfOthDiv) { //scrolled past the other div?
            $("#nav-brand").fadeIn(500); //reached the desired point -- show div
